135675476bfca8adafe4ece0c858d2623598b48e
HinaMuzaffar <hinaa7095@gmail.com>
2025-07-19 12:43:00 +0500

    first commit

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,26 @@
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import { useState } from "react";
+
+export default function WeatherApp() {
+  let [weatherInfo, setWeatherInfo] = useState({
+    city: "Islamabad",
+    weather: "overcast clouds",
+    feelsLike: 30.72,
+    humidity: 70,
+    temp: 28.03,
+    tempMax: 28.03,
+    tempMin: 28.03,
+  });
+
+  let updateInfo = (newInfo) => {
+    setWeatherInfo(newInfo);
+  };
+  return (
+    <div>
+      <h2>Weather App</h2>
+      <SearchBox updateInfo={updateInfo}></SearchBox>
+      <InfoBox info={weatherInfo}></InfoBox>
+    </div>
+  );
+}
